52c9beb0c0489e8fc2d0eeb808521ffa762a332f
Deepu Ajay <deepuajay383@gmail.com>
2024-10-07 22:56:26 -0400

    post method added, get method modified

--- index.js
+++ index.js
@@ -4,14 +4,36 @@ const app = express();
 app.use(express.json()); //interceptor for post requests
 
 const port = 3000;
-const endpoint = "products";
+const postEndpoint = "product";
+const getEndpoint = "products";
 
-app.get(`/${endpoint}`, (req, res) => {
-  res.json({ productId: 123, name: "Candle", price: 12.99, quantity: 12 });
+let datastore = {
+  products: [],
+};
+
+app.get(`/${getEndpoint}`, (req, res) => {
+  res.json(datastore.products);
+});
+
+app.post(`${postEndpoint}`, (req, res) => {
+  const product = req.body;
+  if (
+    !product ||
+    !product.productId ||
+    !product.name ||
+    product.price ||
+    !product.price ||
+    !product.quantity
+  )
+    res.status(400).json({ error: "Invalid data" });
+  datastore.products.push(product);
+  res.status(201).json(product);
 });
 
 app.listen(port, () => {
   console.log(
-    `Server is listening at http://localhost:${port} \nEndpoints: http://localhost:${port}/${endpoint}`
+    `\nServer is listening at http://localhost:${port} \n\nMethod:Post\nEndpoint: http://localhost:${port}/${postEndpoint}
+    \n\n example product object : {"productId": 123, "name":"Candle", "price":12.99, "quantity": 12}
+  \n\nMethod:Get\nEndpoint: http://localhost:${port}/${getEndpoint}`
   );
 });
