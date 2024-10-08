ec9b6f4a6358bdf3536a8b389a6f993491e120bf
Deepu Ajay <deepuajay383@gmail.com>
2024-10-07 23:10:10 -0400

    Delete method added

--- index.js
+++ index.js
@@ -15,25 +15,30 @@ app.get(`/${getEndpoint}`, (req, res) => {
   res.json(datastore.products);
 });
 
-app.post(`${postEndpoint}`, (req, res) => {
+app.post(`/${postEndpoint}`, (req, res) => {
   const product = req.body;
   if (
     !product ||
     !product.productId ||
     !product.name ||
-    product.price ||
     !product.price ||
     !product.quantity
-  )
-    res.status(400).json({ error: "Invalid data" });
+  ) {
+    return res.status(400).json({ error: "Invalid data" });
+  }
   datastore.products.push(product);
   res.status(201).json(product);
+});
+
+app.delete(`/${getEndpoint}`, (req, res) => {
+  datastore.products = [];
+  res.status(200).json({ message: "All products deleted" });
 });
 
 app.listen(port, () => {
   console.log(
     `\nServer is listening at http://localhost:${port} \n\nMethod:Post\nEndpoint: http://localhost:${port}/${postEndpoint}
-    \n\n example product object : {"productId": 123, "name":"Candle", "price":12.99, "quantity": 12}
-  \n\nMethod:Get\nEndpoint: http://localhost:${port}/${getEndpoint}`
+    \n\nexample product object : {"productId": 123, "name":"Candle", "price":12.99, "quantity": 12}
+  \n\nMethod:Get\nEndpoint: http://localhost:${port}/${getEndpoint}\n\nMethod:Delete\nEndpoint: http://localhost:${port}/${getEndpoint}`
   );
 });
